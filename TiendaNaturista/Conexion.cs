86b071ec51e7f78690453aef7cd1dd77f5d58e69
StivenCS <42822488+StivenCS@users.noreply.github.com>
2018-11-06 16:53:44 -0500

    Conexión

---
+++ TiendaNaturista/Conexion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class Conexion
+    {
+     
+        SqlConnection Con = new SqlConnection("Data Source = .; Initial Catalog = TiendaNaturista; Integrated Security = true");
+
+        public void Conectar()
+        {
+            try
+            {
+                Con.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void Desconectar()
+        {
+            Con.Close();
+        }
+
+        //funcion que devuelve la conexion sqlserver
+        public SqlConnection Conex()
+        {
+            return Con;
+        }
+
+    }
+}
