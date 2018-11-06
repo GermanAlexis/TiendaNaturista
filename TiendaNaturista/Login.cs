0db089f0433deb13f36f6ecfe844936bf3f94b7b
StivenCS <42822488+StivenCS@users.noreply.github.com>
2018-11-06 17:42:24 -0500

    Login, Conexion

--- TiendaNaturista/Login.cs
+++ TiendaNaturista/Login.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp;
 
 namespace TiendaNaturista
 {
@@ -15,6 +17,48 @@ namespace TiendaNaturista
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void IngresarLogin_Click(object sender, EventArgs e)
+        {
+            string user = User.Text;
+            string pass = Passwork.Text;
+
+            Conexion Con = new Conexion();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Vendedor where Ven_User=@user and Ven_Password=@password", Con.Conex());
+
+                cmd.Parameters.AddWithValue("@user", user);
+                cmd.Parameters.AddWithValue("@password", pass);
+                Con.Conectar();
+
+                SqlDataAdapter adapt = new SqlDataAdapter(cmd);
+                DataSet dse = new DataSet();
+
+                adapt.Fill(dse);
+                Con.Desconectar();
+
+                int count = dse.Tables[0].Rows.Count;
+
+                if (count == 1)
+                {
+                    MessageBox.Show("Bienvenido al sistema");
+                    this.Hide();
+                    Menu MP = new Menu();
+                    MP.Show();
+                }
+                else
+                {
+                    MessageBox.Show("No existe ningun usuario registrado");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
