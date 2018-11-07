ee9c9895d5eb020227c2e0980bae72a8b3e632e7
Unknown <casegica@gmail.com>
2018-11-06 19:15:44 -0500

    View window product

--- TiendaNaturista/Menu.Designer.cs
+++ TiendaNaturista/Menu.Designer.cs
@@ -64,6 +64,7 @@
             this.button2.Text = "Productos";
             this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
